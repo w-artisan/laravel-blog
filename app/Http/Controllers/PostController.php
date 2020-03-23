d93b1c18d3b7823643928eff1d2fe5b0496272c5
w-artisan <mailtome@i.ua>
2020-03-23 18:21:14 +0200

    using Facades\DB - select data with

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use \App\BlogPost;
 use Illuminate\Http\Request;
 use \App\Http\Requests\StorePost;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -15,6 +16,19 @@ class PostController extends Controller
      */
     public function index()
     {
+        DB::connection()->enableQueryLog();
+
+        $posts = BlogPost::with('comments')->get();
+
+        foreach ($posts as $post) {
+            foreach ($post->comments as $comment) {
+                echo $comment->content;
+                echo '<br>';
+            }
+        }
+
+        dd(DB::getQueryLog());
+
         return view('posts.index', ['posts' => BlogPost::all()] );
     }
 
