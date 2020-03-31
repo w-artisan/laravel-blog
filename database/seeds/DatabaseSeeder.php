34de522dcc5cad15a667c9c7400c9fed3953f0d0
w-artisan <mailtome@i.ua>
2020-03-31 03:16:58 +0300

    Add: DatabaseSeeder to all Tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\User;
+use App\BlogPost;
+use App\Comment;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +15,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(User::class)->states('john-doe')->create();
-        factory(User::class, 20)->create();
+        $doe = factory(User::class)->states('john-doe')->create();
+        $else = factory(User::class, 20)->create();
+
+        $users = $else->concat([$doe]);
+        // dd(get_class($doe), get_class($else));
+        // dd($users->count());
+        $posts = factory(BlogPost::class, 50)->make()->each(function($post) use ($users) {
+            $post->user_id = $users->random()->id;
+            $post->save();
+        });
+
+        $comments = factory(Comment::class, 150)->make()->each(function($comment) use ($posts) {
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });
     }
 }
