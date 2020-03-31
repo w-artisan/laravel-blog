dae1fd24650dddec092ef2ac99e1af49b79a7ee4
w-artisan <mailtome@i.ua>
2020-03-31 13:58:25 +0300

    Refactor: Separate seeders + interactive command line

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\User;
-use App\BlogPost;
-use App\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,21 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $doe = factory(User::class)->states('john-doe')->create();
-        $else = factory(User::class, 20)->create();
+        if ($this->command->confirm('Do you want to refresh the database?')) {
+            $this->command->call('migrate:refresh');
+            $this->command->info('Database was refreshed');
+        };
 
-        $users = $else->concat([$doe]);
-        // dd(get_class($doe), get_class($else));
-        // dd($users->count());
-        $posts = factory(BlogPost::class, 50)->make()->each(function($post) use ($users) {
-            $post->user_id = $users->random()->id;
-            $post->save();
-        });
-
-        $comments = factory(Comment::class, 150)->make()->each(function($comment) use ($posts) {
-            $comment->blog_post_id = $posts->random()->id;
-            $comment->save();
-        });
+        $this->call([
+            UsersTableSeeder::class,
+            BlogPostsTableSeeder::class,
+            CommentsTableSeeder::class,
+            ]);
     }
 }
